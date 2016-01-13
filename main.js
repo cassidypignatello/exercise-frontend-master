db11a01090ae1c50dc88057b75b1692128c6d1b4
cpignatello <cpignatello@yahoo-inc.com>
2016-01-13 16:30:57 -0500

    displays correct show data on top nav click, knows when browser window is resized

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 (function() {
+  var img = document.querySelector('#show-cover');
+  var p = document.querySelector('#episode-count');
+  var h2 = document.querySelector('#show-title'); 
+  var navButtons = document.querySelectorAll('.show-selector');
+  var arr = Array.prototype.slice.call(navButtons);
+
+  document.addEventListener('DOMContentLoaded', function() {
+    checkSize();
+    window.addEventListener('resize', checkSize);
+  });
+
+  function checkSize() {
+    var headerDisplay = window.getComputedStyle(document.querySelector("header"), "display").getPropertyValue("display");
+    if (headerDisplay === "block") {
+      console.log("The screen is bigger!");
+    }
+  }
+
   function fetchJSON(path, callback) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -13,11 +31,8 @@
     xhr.send();
   }
 
-  fetchJSON('shows.json', function(data){
+  fetchJSON('/shows.json', function(data){
     // display first show's info on load
-    var img = document.querySelector('#show-cover');
-    var p = document.querySelector('#episode-count');
-    var h2 = document.querySelector('#show-title'); 
     img.src = data[0].product_image_url;
     p.innerHTML = data[0].episodes + " EPISODES";
     h2.innerHTML = data[0].title.toUpperCase();
@@ -25,12 +40,16 @@
     // add active class to square
     // display correct show number
     
-     
+
     // make nav button background black on click
-    var navButtons = document.querySelectorAll('.show-selector');
-    for (var i = 0; i < navButtons.length; i++) {
-      navButtons[i].addEventListener('click', function() {
+
+    for (var i = 0; i < arr.length; i++) {
+      arr[i].addEventListener('click', function() {
+        var id = arr.indexOf(this);
         this.style.backgroundColor = 'black';
+        img.src = data[id].product_image_url;
+        p.innerHTML = data[id].episodes + " EPISODES";
+        h2.innerHTML = data[id].title.toUpperCase();
       });
     }
 
